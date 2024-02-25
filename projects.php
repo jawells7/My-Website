22dce6cfd15e4ec176d6df2257996a28446e1777
jawells7 <99765399+jawells7@users.noreply.github.com>
2024-02-24 17:02:51 -0700

    Add files via upload

--- projects.php
+++ projects.php
@@ -30,13 +30,13 @@
 			<p>We included several features to ensure data integrity and security. One such feature was password reset. When logging in for the first time using a generic, one-time password, users would immediately be directed to a form to set a new password which adhered to certain stated parameters. We also placed the ability to add and remove employees as well as change data only in the hands of the system administrator who had their own login to the portal. The email for the sys-admin was included in the footer for users to submit change requests.</p>
 			<p>After presenting 2 prototypes and dedicating 5 months of tirelessly researching and refining it, my teammates and I were very proud of our final deliverable and pleased to see the client and professor were proud of our efforts.</p>
 			<a href="https://github.com/jawells7/FedEx-Executive-Dashboard" target="_new"><b>GitHub</b></a><br><br>
-			<b>PDF File</b><br>
+			<a href="./PDF/FedEx Portal/FedEx Executive Portal.pdf" target="_new"><b>PDF File</b></a><br><br>
 		</div>
 		
 		<br>
 		<img border="0" width="49.5%"; height="300px"; src="img/Index.png">
 		<img border="0" width="49.5%"; height="300px"; src="img/Executive Dashboard.png">
-		<br><br>
+		<br><br><hr>
 		<div class="title2">		
 			<p><b><u>Website with Search Capabilities</u></b></p>
 		</div>
@@ -46,12 +46,12 @@
 			<p>For my website, I decided to dedicate it to something I was both familiar with and passionate about: Spirit Halloween. Specifically, I wanted to build a website that served as a database for similarly passionate fans to visit, search the database for information about the company's many animatronic decorations, and read interesting facts and trivia about them.</p>
 			<p>This was a project I thoroughly enjoyed working on not just because I was passionate about the subject matter but I also loved creating and working with the programming languages. I intend on revisiting and expanding on it one of these days, but I'm keeping it on the back-burner for now. With what I've created so far, I'm very proud with how it turned out.</p>
 			<a href="https://github.com/jawells7/Spirit-Website-with-Search-Capabilities" target="_new"><b>GitHub</b></a><br><br>
-			<b>PDF File</b><br>
+			<a href="./PDF/Spirit Website/Spirit Website.pdf" target="_new"><b>PDF File</b></a><br><br>
 		</div>
 		<br>
 		<img border="0" width="49.5%"; height="300px"; src="img/Website Home Page.png">
 		<img border="0" width="49.5%"; height="300px"; src="img/Sign-in page.png">
-		<br><br>
+		<br><br><hr>
 		
 		<div class="title2">
 			<p><b><u>Small Business Data Dashboards</u></b></p>
@@ -63,11 +63,12 @@
 			<p>Once I had finished with the data in Excel and calculating the profits, I uploaded the file into Power BI where I proceeded to create visualizations that represent the data. After testing and playing around with different graphs and settings, I finally completed the dashboard that clearly depicted the trends for profitability throughout each quarter by category. It also allowed the owners to filter the dashboard to view specific a specific quarter and/or category.</p>
 			<p>Upon presenting the final product to the owner, they were very pleased. They were happy with the layout and features of the dashboard as well as excited to be able to see how their company's performing and being able to make appropriate changes. I'm continuing to work with them on analyzing their data and creating visualizations for specific aspects of their company and performance.</p>
 			<p>This project provided an invaluable opportunity for me to put my business analysis education and interest to use and reaffirmed why it's a field I'm pursuing as a career. I had a lot of fun working with the data, creating the visualizations, and reaching conclusions as a result.</p>
-			<b>PDF File</b>
+			<p>And, yes, I did get their permission to use these screenshots for this purpose.</p>
+			<a href="./PDF/BI Dashboard/Power BI Dashboard.pdf" target="_new"><b>PDF File</b></a><br><br>
 		</div>
 		<br>
 		<img border="0" width="100%"; height="500px"; src="img/PowerBI Dashboard.png">
-		<br><br>
+		<br><br><hr>
 		
 		<div class="title2">
 			<p><b><u>This Very Website!</u></b></p>
@@ -79,7 +80,7 @@
 			<p>What originally began as a way to advertise myself and my experience quickly became a passion project the more it developed. The black and gold color-scheme was based off that of my alma mater, UCCS, and the background image of Pikes Peak was one I took. It's a gorgeous view that's greeted me every day during my commute to school since Preschool.</p>
 			<p>I'm pleased with how this project turned out and how it communicates what I have to offer. I hope you've had as much fun viewing and interacting with it as I have had building it.</p>
 			<p>If you're interested in checking out the code, all of the files are over on my GitHub which is linked below.</p>
-			<b>GitHub</b>
+			<a href="https://github.com/jawells7/My-Website" target="_new"><b>GitHub</b></a><br><br>
 		</div>
 		
 	</div>
